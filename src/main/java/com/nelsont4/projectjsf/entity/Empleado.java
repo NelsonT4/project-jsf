5eeba6b11cabf9a731bec3346fd4512db8665040
NelsonT4 <netory4@gmail.com>
2024-12-16 15:15:52 -0500

    Nombre del proyectos: project-jsf
    Descripción: Se integra la herramienta de primefaces y se modifican los
    componentes visuales del login.xhmtl.
    Se agrega una imagen con el componente GraphicImage

---
+++ src/main/java/com/nelsont4/projectjsf/entity/Empleado.java
@@ -0,0 +1,91 @@
+package com.nelsont4.projectjsf.entity;
+/**
+ * @author Nelson
+ * Clase que representa entidades de empleados
+ */
+
+public class Empleado {
+	
+	/**
+	 * Nombre de empleado
+	 */
+	private String nombre;
+	/**
+	 * Primer apellido del empleado
+	 */
+	private String primerApellido;
+	/**
+	 * Segundo apellido del empleado
+	 */
+	private String segundoApellido;
+	/**
+	 * Puesto del empleado
+	 */
+	private String puesto;
+	/**
+	 * Estado actual del empleado
+	 */
+	private boolean estatus;
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the primerApellido
+	 */
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+	/**
+	 * @param primerApellido the primerApellido to set
+	 */
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+	/**
+	 * @return the segundoApellido
+	 */
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+	/**
+	 * @param segundoApellido the segundoApellido to set
+	 */
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+	/**
+	 * @return the puesto
+	 */
+	public String getPuesto() {
+		return puesto;
+	}
+	/**
+	 * @param puesto the puesto to set
+	 */
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	/**
+	 * @return the estatus
+	 */
+	public boolean isEstatus() {
+		return estatus;
+	}
+	/**
+	 * @param estatus the estatus to set
+	 */
+	public void setEstatus(boolean estatus) {
+		this.estatus = estatus;
+	}
+	
+	
+}
